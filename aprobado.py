7605fc6f741dc6352ab3ba0fe3ff563cf5b12bb4
Richard <richardrlvt@gmail.com>
2020-07-09 15:09:43 -0500

    Ejercicio para determinar aprobado listo

--- aprobado.py
+++ aprobado.py
@@ -1,2 +1,14 @@
 #Ejercicio que determine si un estudiante esta arpobado o no 
 # Autor :Richard Victorio Tito
+
+
+def determinaraprobado(promedio):
+    if promedio >=11:
+        resultado ="Aprobado"
+    else:
+        resultado="Desaprobado"
+    return resultado
+promedio= int(input("Promedio:"))
+print(determinaraprobado(promedio))
+
+
